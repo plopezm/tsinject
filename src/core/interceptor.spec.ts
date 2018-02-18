db6f9bb79701af8600ede9839c2b9f415086d89c
Pablo LM <pabloplm@gmail.com>
2018-02-18 13:47:56 +0100

    Implementing multiple interceptors

--- src/core/interceptor.spec.ts
+++ src/core/interceptor.spec.ts
@@ -9,37 +9,63 @@ describe('Interceptors', () => {
 
     @Interceptor
     class ExampleInterceptor implements InterceptorComponent{
-        data: any = {};
-        constructor(){
-        }                
         invoke(this: InterceptorProperties): any {
             let result = this.next(...this.args);
-            return `PRE${result}POST`;
+            return `[${result}]`;
+        }
+    }
+
+    @Interceptor
+    class ExampleInterceptor2 {
+        invoke(this: InterceptorProperties): any {
+            let result = this.next(...this.args);
+            return `<${result}>`;
         }
     }
 
     class ExampleIntercepted {
         @Intercepted(ExampleInterceptor)
         getHelloString(): string {
-            console.log('METHOD CALLED');
-            return 'hello';
+            return 'Hello';
         }
 
         @Intercepted(ExampleInterceptor)
         getNamedHelloString(name: string): string {
-            return `Hello-Mr-${name}`;
+            return `Hello Mr ${name}`;
+        }
+
+        @Intercepted(ExampleInterceptor)
+        getNamedHelloWithSurnameString(name: string, surname: string, surname2: string): string {
+            return `Hello Mr ${name} ${surname} ${surname2}`;
+        }
+
+        @Intercepted(ExampleInterceptor, ExampleInterceptor2)
+        getHelloString2(): string {
+            return 'Hello';
         }
     }
     
     it('Interceptor is executed with function without parameters', () => {
         let obj = new ExampleIntercepted();
         const result = obj.getHelloString();
-        expect(result).to.equals('PREhelloPOST');
+        expect(result).to.equals('[Hello]');
     });
 
     it('Interceptor is executed with function with parameters', () => {
         let obj = new ExampleIntercepted();
-        const result = obj.getNamedHelloString('pablo');
-        expect(result).to.equals('PREHello-Mr-pabloPOST');
+        const result = obj.getNamedHelloString('Pablo');
+        expect(result).to.equals('[Hello Mr Pablo]');
+    });
+
+    it('Interceptor is executed with a lot of parameters', () => {
+        let obj = new ExampleIntercepted();
+        const result = obj.getNamedHelloWithSurnameString('Pablo', 'Lopez', 'Martinez');
+        expect(result).to.equals('[Hello Mr Pablo Lopez Martinez]');
+    });
+
+    it('Multiple interceptors are executed', () => {
+        let obj = new ExampleIntercepted();
+        const result = obj.getHelloString2();
+        expect(result).to.equals('<[Hello]>');
     });
 });
--- src/core/interceptor.ts
+++ src/core/interceptor.ts
@@ -15,23 +15,25 @@ interface ProxyPerformerProperties {
     target: Function;
 }
 
-// TODO: Declares an interceptor
-export function Interceptor(interceptor: Function | InterceptorComponent) {
-    console.log("Interceptor: ", interceptor);
-    console.log("invoke: ", interceptor.prototype.invoke);
+// It declares an interceptor
+export function Interceptor(interceptor: InterceptorComponent | any) {
+    if (!interceptor.prototype.invoke) {
+        throw Error("An interceptor class MUST implements interface InterceptorComponent");
+    }
     InjectionFactory.registerInstance(interceptor.name, interceptor.prototype.invoke);
 }
 
-// TODO: Binds method with interceptor
-export function Intercepted(clazzes: any) {
+// It binds method with interceptor
+export function Intercepted(...clazzes: any[]) {
     return function(target: any, key: string, descriptor: PropertyDescriptor): any {
-        let interceptor = InjectionFactory.getSingleton(clazzes.name);
-        let performer = performInterception.bind({interceptor: interceptor, target: target[key]});
-        descriptor.value = performer;
+        for (let clazz of clazzes) {
+            let interceptor = InjectionFactory.getSingleton(clazz.name);
+            let performer = performInterception.bind({interceptor: interceptor, target: target[key]});
+            descriptor.value = performer;
+        }
     }
 }
 
 const performInterception = function(this: ProxyPerformerProperties, ...args: any[]){
-    console.log("performInterception", "this =", this);
     return this.interceptor.apply({next: this.target, args: args});
 }
