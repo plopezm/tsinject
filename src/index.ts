f1566c6c11c953c946122eff05f8f7ab8551d525
Pablo Lopez Martinez <plomarti@everis.com>
2018-02-26 11:57:45 +0100

    exported injectable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Inject, Produces } from './core/inject';
+export { Injectable, Inject, Produces } from './core/inject';
 export { InjectionFactory } from './store/injection-factory';
 export { Interceptor, Intercepted, InterceptorComponent, NextInterceptor } from './core/interceptor';
